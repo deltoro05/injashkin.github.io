02082f8aed3ad5e257cbb0a40b7df429d877e24a
injashkin <injashkin@gmail.com>
2022-09-11 07:13:36 +0300

    Separated settings for production and development

---
+++ rollup.config.dev.js
@@ -0,0 +1,13 @@
+export default {
+  input: 'src/index.js',
+
+  watch: {
+    include: './src/**',
+    clearScreen: false,
+  },
+
+  output: {
+    file: 'dev/index.js',
+    format: 'iife',
+  },
+};
