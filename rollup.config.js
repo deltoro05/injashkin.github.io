e5da4cdadb174eb0e80bffac3d8ed3eab9bda444
injashkin <injashkin@gmail.com>
2022-10-14 17:54:26 +0300

    Update article

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default {
   },
 
   output: {
-    file: 'docs/index.1gf919tc4.js', //- эта строка обрабатывается регулярным выражением в scripts.js
+    file: 'docs/index.1gfbfkpu2.js', //- эта строка обрабатывается регулярным выражением в scripts.js
     format: 'iife',
   },
 };
