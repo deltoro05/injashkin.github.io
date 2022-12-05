5640b175586660685de2544cd55a70489d2a1f97
injashkin <injashkin@gmail.com>
2022-12-05 21:39:26 +0300

    Update site

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -7,7 +7,7 @@ export default {
   },
 
   output: {
-    file: 'dev/index.1gj1hs667.js', //- эта строка обрабатывается регулярным выражением в scripts.js
+    file: 'dev/index.1gjhpaa2l.js', //- эта строка обрабатывается регулярным выражением в scripts.js
     format: 'iife',
   },
 };
