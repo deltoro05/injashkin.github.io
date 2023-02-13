e79e30429431ccd636cb5ab4484de51cfce7dda6
injashkin <injashkin@gmail.com>
2023-02-13 15:46:44 +0300

    Update site

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default {
   },
 
   output: {
-    file: 'docs/index.1gp5cqbpv.js', //- эта строка обрабатывается регулярным выражением в scripts.js
+    file: 'docs/index.1gp5cs5pm.js', //- эта строка обрабатывается регулярным выражением в scripts.js
     format: 'cjs',
   },
 };
